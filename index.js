bdda06f5bfa8ee95a8bc2ffdacc7adf8273d82cb
s.limguy <shuanmlim@icloud.com>
2022-07-20 17:42:39 -0700

    html, template.js, added to index.js

--- index.js
+++ index.js
@@ -12,6 +12,31 @@ teamArray = [];
 function runApp () {
 // Add OOP functions here - create for the other roles, but not Employee.
 
+
+function createTeam () {
+    inquirer.prompt([{
+      type: "list",
+      message: "What type of employee would you like to add to your team?",
+      name: "addEmployeePrompt",
+      choices: ["Manager", "Engineer", "Intern", "No more team members are needed."]
+    }]).then(function (userInput) {
+      switch(userInput.addEmployeePrompt) {
+        case "Manager":
+          addManager();
+          break;
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+
+        default:
+          htmlBuilder();
+      }
+    })
+  }
+  
 function addManager() {
     inquirer.prompt ([
   
@@ -117,8 +142,16 @@ function addEngineer() {
   }
 
   
-  function createTeam () {
+  function htmlBuilder () {
     console.log("Team created!")
-    }
+    
+
+    fs.writeFileSync(outputPath, generateTeam(teamArray), "UTF-8")
+  }
+
+  createTeam();
+
 
 }
+
+ runApp();
